437f6b6376fa69b0bbf756a1f841a1ab4198ba27
Sara <42779113+S-Asghari@users.noreply.github.com>
2026-08-12 21:08:21 -0700

    Time: 7 ms (90.31%), Space: 20.6 MB (39.52%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -8,8 +8,8 @@ class Solution:
             else: # c == ')'
                 stack.pop()
                 if len(stack) == 0:
-                    stack.append(i)
+                    stack.append(i) # We push i as the new base marker, since nothing before or including this point can be part of a valid substring going forward.
                 else:
                     ans = max(ans, i - stack[-1])
-                    
+
         return ans
