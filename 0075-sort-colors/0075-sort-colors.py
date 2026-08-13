5490327c9f9812b134bc2e1ec8a13b416e852de5
Sara <42779113+S-Asghari@users.noreply.github.com>
2026-08-13 14:39:21 -0700

    Time: 0 ms (100%), Space: 19.4 MB (22.41%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,18 +3,39 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # counting sort
-        colors = [0] * 3
-        for num in nums:
-            colors[num] += 1
+        # -------------
+        # Counting Sort
+        # -------------
+        # colors = [0] * 3
+        # for num in nums:
+        #     colors[num] += 1
         
-        i, j = 0, 0
-        while i < len(nums):
-            if colors[j] > 0:
-                nums[i] = j
-                i += 1
-                colors[j] -= 1
-            else:
-                j += 1
+        # i, j = 0, 0
+        # while i < len(nums):
+        #     if colors[j] > 0:
+        #         nums[i] = j
+        #         i += 1
+        #         colors[j] -= 1
+        #     else:
+        #         j += 1
+
+        # return nums
+
+        # ----------
+        # Quick Sort
+        # ----------
+        l, r = 0, len(nums)-1
+        i = 0
+
+        while i <= r:
+            # partition value = 1
+            if nums[i] < 1:
+                nums[l], nums[i] = nums[i], nums[l]
+                l += 1
+            elif nums[i] > 1:
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+                i -= 1 # Don't increament i
+            i += 1
 
         return nums
