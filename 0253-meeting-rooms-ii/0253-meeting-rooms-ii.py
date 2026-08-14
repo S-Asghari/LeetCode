774a6cfb035f5b37c0a2054e7bab9246c8a064dc
Sara <42779113+S-Asghari@users.noreply.github.com>
2026-08-13 18:57:13 -0700

    Time: 7 ms (24.47%), Space: 23.9 MB (5.3%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -5,7 +5,7 @@ class Solution:
             times.append((s, 's'))
             times.append((e, 'e'))
         times = sorted(times, key=lambda x: (x[0], x[1]))
-        print(times)
+
         rooms = 0
         
         curRooms = 0
