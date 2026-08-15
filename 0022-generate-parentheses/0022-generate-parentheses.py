ce9a6b7c772b7266a81174ae05de48a3d86415a9
Sara <42779113+S-Asghari@users.noreply.github.com>
2026-08-14 18:18:59 -0700

    Time: 0 ms (100%), Space: 19.5 MB (9.11%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -5,14 +5,36 @@ class Solution:
         # n = 1: ()
         # n = 2: (()), ()()
         # n = 3: ((())), (())(), ()(()), (()()), ()()()
-        outputs = defaultdict(set)
-        outputs[1] = set(["()"]) 
-        for i in range(2, n+1):
-            for j in range(1, i):
-                for s1 in outputs[j]:
-                    for s2 in outputs[i-j]:
-                        outputs[i].add(s1+s2)
-            for s in outputs[i-1]:
-                outputs[i].add(f"({s})")
+        # ----------
+        # Solution 1
+        # ----------
+        # outputs = defaultdict(set)
+        # outputs[1] = set(["()"]) 
+        # for i in range(2, n+1):
+        #     for j in range(1, i):
+        #         for s1 in outputs[j]:
+        #             for s2 in outputs[i-j]:
+        #                 outputs[i].add(s1+s2)
+        #     for s in outputs[i-1]:
+        #         outputs[i].add(f"({s})")
             
-        return list(outputs[n])
+        # return list(outputs[n])
+        # ----------
+        # Solution 2
+        # ----------
+        res = []
+        def backtrack(s, onum, cnum):
+            # print(f"s:{s}, onum:{onum}, cnum:{cnum}")
+            if onum == n and cnum == n:
+                res.append(s)
+                return
+            
+            if onum == cnum:
+                backtrack(s+"(", onum+1, cnum)
+            elif onum > cnum:
+                if onum < n:
+                    backtrack(s+"(", onum+1, cnum)
+                backtrack(s+")", onum, cnum+1)
+
+        backtrack("", 0, 0)
+        return res
