14832eb6cca341be594397d062f2e71024850373
Sara <42779113+S-Asghari@users.noreply.github.com>
2026-08-15 16:10:23 -0700

    Time: 111 ms (64.34%), Space: 38 MB (54.72%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,19 +6,52 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        stackA = []
-        while headA:
-            stackA.append(headA)
-            headA = headA.next 
+        # -----------
+        # Solution A
+        # -----------
+        # stackA = []
+        # while headA:
+        #     stackA.append(headA)
+        #     headA = headA.next 
 
-        stackB = []
-        while headB:
-            stackB.append(headB)
-            headB = headB.next
+        # stackB = []
+        # while headB:
+        #     stackB.append(headB)
+        #     headB = headB.next
         
-        Intersection = None
-        while stackA and stackB and stackA[-1] == stackB[-1]:
-            Intersection = stackA.pop()
-            stackB.pop()
+        # Intersection = None
+        # while stackA and stackB and stackA[-1] == stackB[-1]:
+        #     Intersection = stackA.pop()
+        #     stackB.pop()
         
-        return Intersection
+        # return Intersection
+        # -----------
+        # Solution B
+        # -----------
+        lenA, lenB = 0, 0
+        nodeA, nodeB = headA, headB
+        while nodeA:
+            lenA += 1
+            nodeA = nodeA.next
+        while nodeB:
+            lenB += 1
+            nodeB = nodeB.next
+        
+        nodeA, nodeB = headA, headB
+        
+        if lenA > lenB:
+            diff = lenA - lenB
+            while diff:
+                nodeA = nodeA.next
+                diff -= 1
+        elif lenB > lenA:
+            diff = lenB - lenA
+            while diff:
+                nodeB = nodeB.next
+                diff -= 1
+        
+        while nodeA and nodeA != nodeB:
+            nodeA = nodeA.next
+            nodeB = nodeB.next
+        
+        return nodeA
