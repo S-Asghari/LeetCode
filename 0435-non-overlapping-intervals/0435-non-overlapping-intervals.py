bc5d0d28a51a1d339be26f998b550158fccccf13
Sara <42779113+S-Asghari@users.noreply.github.com>
2026-08-15 20:41:36 -0700

    Time: 171 ms (18.54%), Space: 49.1 MB (36.15%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -7,14 +7,10 @@ class Solution:
         while r < len(intervals):
             if intervals[r][0] < intervals[l][1]: # There's an overlap
                 res += 1
-                if intervals[r][1] > intervals[l][1]:
-                    r += 1
-                else: # intervals[l][1] >= intervals[r][1]
+                if intervals[l][1] >= intervals[r][1]:
                     l = r
-                    r += 1
             else:
                 l = r
-                r += 1
-        return res
+            r += 1
 
         return res
