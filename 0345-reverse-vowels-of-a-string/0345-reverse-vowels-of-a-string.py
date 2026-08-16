fd8d99a40bcedaeae77d41ffb2ed1aedc8578a02
Sara <42779113+S-Asghari@users.noreply.github.com>
2026-08-15 19:01:38 -0700

    Time: 8 ms (65.01%), Space: 20.5 MB (51.61%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -1,14 +1,33 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
+        # ----------
+        # Solution A
+        # ----------
+        # vowels = set(['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'])
+        # stack = []
+        # for c in s:
+        #     if c in vowels:
+        #         stack.append(c)
+        # new_s = []
+        # for i, c in enumerate(s):
+        #     if c in vowels:
+        #         new_s.append(stack.pop())
+        #     else:
+        #         new_s.append(c)
+        # return ''.join(new_s)
+        # ----------
+        # Solution B
+        # ----------
+        s = list(s)
+        l, r = 0, len(s)-1
         vowels = set(['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'])
-        stack = []
-        for c in s:
-            if c in vowels:
-                stack.append(c)
-        new_s = []
-        for i, c in enumerate(s):
-            if c in vowels:
-                new_s.append(stack.pop())
+        while l <= r:
+            while l < len(s) and s[l] not in vowels: l += 1
+            while r >= 0 and s[r] not in vowels: r -= 1
+            if l > r: break
             else:
-                new_s.append(c)
-        return ''.join(new_s)
+                s[l], s[r] = s[r], s[l]
+                l += 1
+                r -= 1
+
+        return ''.join(s)
