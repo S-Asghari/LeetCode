5f9802599484a7e2d5ec8686cc0cb96d088d403e
Sara <42779113+S-Asghari@users.noreply.github.com>
2026-08-23 20:26:10 -0700

    Time: 3 ms (18.23%), Space: 19.2 MB (61.59%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,9 +1,36 @@
+from string import ascii_lowercase as chars
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        # -----------
         # Brute-Force
+        # -----------
         n, m = len(haystack), len(needle)
         if m == 0: return 0
-        for i in range(0, n-m+1):
-            if haystack[i:i+m] == needle:
-                return i
+        if n < m: return -1
+        # for i in range(0, n-m+1):
+        #     if haystack[i:i+m] == needle:
+        #         return i
+        # return -1
+        # ----------
+        # Rabin Karp
+        # ----------
+        mod = 10 ** 9 + 7
+        d = {char: i for i, char in enumerate(chars)}
+        needleVal = 0
+        for char in needle:
+            needleVal = ((needleVal * 26) % mod + d[char]) % mod
+        
+        substrVal = 0
+        for char in haystack[:m]:
+            substrVal = ((substrVal * 26) % mod + d[char]) % mod
+        if substrVal == needleVal:
+            return 0
+        
+        for i in range(m, n):
+            pos = (26 ** (m-1)) % mod
+            substrVal = substrVal - (d[haystack[i-m]] * pos) % mod
+            substrVal = ((substrVal * 26) % mod + d[haystack[i]]) % mod
+            if substrVal == needleVal:
+                return i - m + 1
+        
         return -1
