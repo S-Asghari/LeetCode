df5e2e381484822fe65bd208d875088acdd7f9a1
Sara <42779113+S-Asghari@users.noreply.github.com>
2026-09-05 20:58:21 -0700

    Time: 0 ms (100%), Space: 19.9 MB (9.47%) - LeetHub

---
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -0,0 +1,39 @@
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        # NeetCode's Solution
+        adj = {c: set() for w in words for c in w}
+
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i+1]
+            minLen = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:minLen] == w2:
+                return ""
+            for j in range(minLen):
+                if w1[j] != w2[j]:
+                    adj[w1[j]].add(w2[j])
+                    break
+        
+        visited = {} # False: visited before, True: visited in the current path
+        res = []
+
+        def dfs(c):
+            if c in visited:
+                # If visited[c] == True: c is in the current path => Found a loop
+                # If visited[c] == False: All paths starting from c have been traversed before
+                return visited[c]
+            
+            visited[c] = True
+            
+            for nei in adj[c]:
+                if dfs(nei):
+                    return True
+            
+            visited[c] = False
+            res.append(c)
+        
+        for c in adj:
+            if dfs(c):
+                return ""
+        
+        res.reverse()
+        return "".join(res)
