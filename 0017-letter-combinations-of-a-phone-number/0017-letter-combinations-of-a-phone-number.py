4766f974ae4dac146703a455c7db4e97471e9862
Sara <42779113+S-Asghari@users.noreply.github.com>
2026-09-08 21:12:51 -0700

    Time: 0 ms (100%), Space: 19.3 MB (78.46%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,11 +1,29 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        mapping = {2: "abc", 3: "def", 4: "ghi", 5: "jkl", 6: "mno", 7: "pqrs", 8: "tuv", 9: "wxyz"}
-        res = [""]
-        for d in digits:
-            temp_res = []
-            for letter in res:
-                for c in mapping[int(d)]:
-                    temp_res.append(letter + c)
-            res = temp_res
+        # ----------
+        # Solution 1
+        # ----------
+        # mapping = {2: "abc", 3: "def", 4: "ghi", 5: "jkl", 6: "mno", 7: "pqrs", 8: "tuv", 9: "wxyz"}
+        # res = [""]
+        # for d in digits:
+        #     temp_res = []
+        #     for letter in res:
+        #         for c in mapping[int(d)]:
+        #             temp_res.append(letter + c)
+        #     res = temp_res
+        # return res
+        # ----------
+        # Solution 2
+        # ----------
+        res = []
+        mapping = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        def backtrack(d, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            
+            for c in mapping[digits[d]]:
+                backtrack(d + 1, curStr + c)
+        
+        backtrack(0, "")
         return res
