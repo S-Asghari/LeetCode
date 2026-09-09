5be1964e5a94976dc651fbfa3ff733317630615f
Sara <42779113+S-Asghari@users.noreply.github.com>
2026-09-08 17:32:23 -0700

    Time: 154 ms (10.99%), Space: 35.1 MB (31.11%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,8 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # ----------
+        # Solution 1
+        # ----------
         n = len(nums)
         k %= n
-        # nums[0: n] == nums[0:n-k] + nums[n-k:n]
-        nums[:] = nums[n-k:n] + nums[0:n-k]
+        # # nums[0: n] == nums[0:n-k] + nums[n-k:n]
+        # nums[:] = nums[n-k:n] + nums[0:n-k]
+        # ----------
+        # Solution 2
+        # ----------
+        def reverse(l, r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
         
+        reverse(0, n-1)
+        reverse(0, k-1)
+        reverse(k, n-1)
