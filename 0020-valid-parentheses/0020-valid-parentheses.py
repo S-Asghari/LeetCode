37d435ea697421e3df76e24e5e83b056be7a4830
Sara <42779113+S-Asghari@users.noreply.github.com>
2026-09-08 20:47:20 -0700

    Time: 3 ms (32.24%), Space: 19.3 MB (63.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkMatch(self, c1: str, c2: str) -> bool:
+        if c1 == '(' and c2 == ')': return True  
+        if c1 == '[' and c2 == ']': return True 
+        if c1 == '{' and c2 == '}': return True
+        return False
+
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for i in range(len(s)):
+            if s[i] == '(' or s[i] == '[' or s[i] == '{':
+                stack.append(s[i])
+            else:
+                if not stack: return False
+                if not self.checkMatch(stack.pop(), s[i]): return False
+        if not stack: 
+            return True
+        else:
+            return False
